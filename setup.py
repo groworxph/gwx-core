ae56a801c1df44c83a33d301da2cac183fd1f941
Jerric Calosor <jerric.calosor@groworx.com.au>
2020-03-09 07:20:50 +0800

    release/0.0.3
    - Candidate for version release 0.0.3
    - Bump the version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="gwx-core",
-    version="0.0.2",
+    version="0.0.3",
     author="Jerric Calosor",
     author_email="jerric.calosor@groworx.com.au",
     description="Core libraries and modules for flask projects.",
